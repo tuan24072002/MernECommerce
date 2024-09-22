6b664d6f46517eeb4bbaf573fd9ee301febad6d5
tuan24072002 <44106906+tuan24072002@users.noreply.github.com>
2024-09-22 11:20:01 +0700

    new project

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use('/api/coupon', couponRoute);
 app.use('/api/payment', paymentRoute);
 app.use('/api/analytic', analyticRoute);
 
-if (process.env.MODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
